8fdd39a5cd780dd8f5341fca108e3d8e002608ab
eli <eliadadearaujolinhares@gmail.com>
2025-10-10 21:47:08 -0300

    Add files via upload

---
+++ atividade12.py
@@ -0,0 +1,6 @@
+# Crie uma lista de números de 1 a 10 e mostre apenas os números pares.
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for numero in numeros:
+    if numero % 2 == 0:
+        print(numero)
